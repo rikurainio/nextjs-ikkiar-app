8fc5326d7b642da17a12736e6f41d2fa55dc586c
Riku Rainio <rkurainio@gmail.com>
2022-05-01 16:18:01 +0300

    add pointer-cursor to navbar item divs

--- components/navbar.js
+++ components/navbar.js
@@ -6,21 +6,21 @@ const Navbar = () => {
     <div className='flex justify-center space-x-10 mt-16'>
 
       <Link href='/leaderboard'>
-        <div className='bg-ikkiarBright rounded-full w-52 h-8 shadow-sm border- text-center hover:scale-105 hover:bg-ikkiarDark2 transition-all'>
+        <div className='cursor-pointer bg-ikkiarBright rounded-full w-52 h-8 shadow-sm border- text-center hover:scale-105 hover:bg-ikkiarDark2 transition-all'>
           <h1 className="text-xl font-semibold">
             🐵 Leaderboard
           </h1>
         </div>
       </Link>
       <Link href='/'>
-        <div className='bg-ikkiarBright rounded-full w-52 h-8 shadow-sm text-center hover:scale-105 hover:bg-ikkiarDark2 transition-all'>
+        <div className='cursor-pointer bg-ikkiarBright rounded-full w-52 h-8 shadow-sm text-center hover:scale-105 hover:bg-ikkiarDark2 transition-all'>
         <h1 className="text-xl font-bold">
             🐒 Home
           </h1>
         </div>
       </Link>
       <Link href='/history'>
-        <div className='bg-ikkiarBright rounded-full w-52 h-8 shadow-sm text-center hover:scale-105 hover:bg-ikkiarDark2 transition-all'>
+        <div className='cursor-pointer bg-ikkiarBright rounded-full w-52 h-8 shadow-sm text-center hover:scale-105 hover:bg-ikkiarDark2 transition-all'>
         <h1 className="text-xl font-bold">
             🙈 Match history
           </h1>
