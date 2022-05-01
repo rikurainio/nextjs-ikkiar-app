ea09031d101cd4ea4ac6e09651bfdfe874e1c11c
Riku Rainio <rkurainio@gmail.com>
2022-05-01 13:12:44 +0300

    add loading spinners

--- pages/history.js
+++ pages/history.js
@@ -3,14 +3,17 @@ import matchService from '../services/matches'
 import { RiSwordFill } from 'react-icons/ri';
 import { motion } from "framer-motion"
 import { getChampionImageSource, getChampionNameById } from '../services/champion'
+import RotateLoader from "react-spinners/RotateLoader";
 
 const History = () => {
+  const [loading, setLoading] = useState(true)
   const [matches, setMatches] = useState([])
   const [winner, setWinner] = useState(0)
 
   useEffect(async () => {
     const matches = await matchService.getAll()
     setMatches(matches)
+    setLoading(false)
   }, [])
 
 
@@ -51,139 +54,94 @@ const History = () => {
 
   console.log('setmatches',matches)
 
-  return (
-    <div className='bg-ikkiarBgGray2 flex-col justify-center mx-128 mt-10 rounded-md overflow-hidden'>
-      <div className=''>
-        <h2 className='text-2xl text-center'>
-          Match history
-        </h2>
-
-        <div className='flex-col p-6'>
-          {matches.map((match, idx) => 
-            <motion.div whileHover={{ scale: 1.03 }}  key={'leaderboard-summoner-' + idx} className={'flex justify-end w-auto h-auto border-l-8 border-green rounded-l py-2 my-12 bg-ikkiarBgGray1'}>
-
-            <div className='flex-col pr-12 border-r border-grayDetailText mr-3'>
-              <div className='truncate'>
-                {getTimeStamp(match.gameData.info.gameCreation)}
-              </div>
-              <div className='truncate'>
-                {(match.gameData.info.gameDuration / 60).toFixed()} minutes 
-              </div>
-              <div className={'truncate font-extralight' + getCssRuleWin(match.gameData.info.teams[0])}>
-                {getWinnerText(match.gameData.info.teams[0])} Win 
-              </div>
-              <div className='truncate text-grayDetailText text-xs font-thin'>
-                ver:{match.gameData.info.gameVersion}
-              </div>
-            </div>
-
-            <div className='flex-col pr-3'>
-              <p className='text-blue text-lg'>{match.gameData.info.teams[0].objectives.champion.kills}</p>
-              <RiSwordFill className='text-xl'></RiSwordFill>
-              <p className='text-red text-lg'>{match.gameData.info.teams[0].objectives.champion.kills}</p>
-            </div>
-          
-            <div className='flex-col'>
-                  <div className='flex'>
-                    <div className='flex mr-4'>
-                      {match.gameData.info.participants.filter(p => p.teamId === 100).map((participant, idx) =>
-                              <div key={'blue-summoner-'+idx} className='flex-row mr-1'>
-                                    <img className='object-scale-down w-14 rounded-none border-l-8 border-blue' src={getChampionImageSource(participant.championName)}></img>
-                                    <div className='w-14 text-left text-xs truncate'>
-                                      <p>{participant.summonerName}</p>
-                                    </div>
-                              </div>
-                        )}
-                    </div>
-                    <div className='flex'>
-                      {match.gameData.info.participants.filter(p => p.teamId === 200).map((participant, idx) =>
-                              <div key={'red-summoner-'+idx} className='flex-row mr-1'>
-                                    <img className='object-scale-down w-14 rounded-none border-r-8 border-red' src={getChampionImageSource(participant.championName)}></img>
-                                    <div className='w-14 text-left text-xs truncate'>
-                                      <p>{participant.summonerName}</p>
-                                    </div>
-                              </div>
-                        )}
-                    </div>
-                  </div>
-              <div className='flex justify-between'>
-                    <div className='flex m-1'>
-                      {match.gameData.info.teams['0'].bans.map((ban, idx) => 
-                        <div key={'blue-ban-'+idx}>
-                          <img className='object-scale-down w-6 rounded-none' src={getChampionImageSource(getChampionNameById(ban.championId))}></img>
-                        </div>
-                        )}
-                    </div>
-                    <div className='flex m-1'>
-                      {match.gameData.info.teams['1'].bans.map((ban, idx) => 
-                        <div key={'red-ban-'+idx} >
-                          <img className='object-scale-down w-6 rounded-none' src={getChampionImageSource(getChampionNameById(ban.championId))}></img>
-                        </div>
-                        )}
-                    </div>
-                  </div>
-            </div>
-
-          
-           
 
 
+  if(!loading){
+    return (
+      <div className='bg-ikkiarBgGray2 flex-col justify-center mx-128 mt-10 rounded-md overflow-hidden'>
+        <div className=''>
+          <h2 className='text-2xl text-center pt-5'>
+            Match history
+          </h2>
 
-            {/* 
-            <div className='flex-col shadow-sm'>
-                <div className='flex justify-between mx-1'>
-                  <div className='flex'>
-                    {match.gameData.info.teams['0'].bans.map(ban => 
-                      <div className=''>
-                        <img className='object-scale-down w-6 rounded-none' src={getChampionImageSource(getChampionNameById(ban.championId))}></img>
-                      </div>
-                      )}
-                  </div>
-                  <div className='flex'>
-                    {match.gameData.info.teams['1'].bans.map(ban => 
-                      <div>
-                        <img className='object-scale-down w-6 rounded-none' src={getChampionImageSource(getChampionNameById(ban.championId))}></img>
-                      </div>
-                      )}
-                  </div>
+          <div className='flex-col p-6'>
+            {matches.map((match, idx) => 
+              <motion.div whileHover={{ scale: 1.03 }}  key={'leaderboard-summoner-' + idx} className={'flex justify-end w-auto h-auto border-l-8 border-green rounded-l py-2 my-12 bg-ikkiarBgGray1'}>
+
+              <div className='flex-col pr-12 border-r border-grayDetailText mr-3'>
+                <div className='truncate'>
+                  {getTimeStamp(match.gameData.info.gameCreation)}
                 </div>
-
-                <div className='flex'>
-                  <div>
-                    {match.gameData.info.participants.filter(p => p.teamId === 100).map(participant =>
-                      <div className='flex-row p-1'>
-                            <img className='object-scale-down w-14 rounded-none border-l-8 border-blue' src={getChampionImageSource(participant.championName)}></img>
-                            <div className='w-32 text-center'>
-                              <p>{participant.summonerName}</p>
-                            </div>
-                      </div>
-                    )}
-                  </div>
-                  <div>
-                    {match.gameData.info.participants.filter(p => p.teamId === 200).map(participant =>
-                      <div className='flex-col p-1'>
-                            <img className='object-scale-down w-14 rounded-none border-r-8 border-red' src={getChampionImageSource(participant.championName)}></img>
-                            <div className='w-32 text-center'>
-                              <p>{participant.summonerName}</p>
-                            </div>
-                      </div>
-                    )}
-                  </div>
+                <div className='truncate'>
+                  {(match.gameData.info.gameDuration / 60).toFixed()} minutes 
                 </div>
-            </div>
-      */}
-           
-           
-          </motion.div>
+                <div className={'truncate font-extralight' + getCssRuleWin(match.gameData.info.teams[0])}>
+                  {getWinnerText(match.gameData.info.teams[0])} Win 
+                </div>
+                <div className='truncate text-grayDetailText text-xs font-thin'>
+                  ver:{match.gameData.info.gameVersion}
+                </div>
+              </div>
 
-          
+              <div className='flex-col pr-3'>
+                <p className='text-blue text-lg'>{match.gameData.info.teams[0].objectives.champion.kills}</p>
+                <RiSwordFill className='text-xl'></RiSwordFill>
+                <p className='text-red text-lg'>{match.gameData.info.teams[0].objectives.champion.kills}</p>
+              </div>
             
-          )}
+              <div className='flex-col'>
+                    <div className='flex'>
+                      <div className='flex mr-4'>
+                        {match.gameData.info.participants.filter(p => p.teamId === 100).map((participant, idx) =>
+                                <div key={'blue-summoner-'+idx} className='flex-row mr-1'>
+                                      <img className='object-scale-down w-14 rounded-none border-l-8 border-blue' src={getChampionImageSource(participant.championName)}></img>
+                                      <div className='w-14 text-left text-xs truncate'>
+                                        <p>{participant.summonerName}</p>
+                                      </div>
+                                </div>
+                          )}
+                      </div>
+                      <div className='flex'>
+                        {match.gameData.info.participants.filter(p => p.teamId === 200).map((participant, idx) =>
+                                <div key={'red-summoner-'+idx} className='flex-row mr-1'>
+                                      <img className='object-scale-down w-14 rounded-none border-r-8 border-red' src={getChampionImageSource(participant.championName)}></img>
+                                      <div className='w-14 text-left text-xs truncate'>
+                                        <p>{participant.summonerName}</p>
+                                      </div>
+                                </div>
+                          )}
+                      </div>
+                    </div>
+                <div className='flex justify-between'>
+                      <div className='flex m-1'>
+                        {match.gameData.info.teams['0'].bans.map((ban, idx) => 
+                          <div key={'blue-ban-'+idx}>
+                            <img className='object-scale-down w-6 rounded-none' src={getChampionImageSource(getChampionNameById(ban.championId))}></img>
+                          </div>
+                          )}
+                      </div>
+                      <div className='flex m-1'>
+                        {match.gameData.info.teams['1'].bans.map((ban, idx) => 
+                          <div key={'red-ban-'+idx} >
+                            <img className='object-scale-down w-6 rounded-none' src={getChampionImageSource(getChampionNameById(ban.championId))}></img>
+                          </div>
+                          )}
+                      </div>
+                    </div>
+              </div>
+            </motion.div>
+              
+            )}
+          </div>
         </div>
-
       </div>
-    
-    
+    )
+  }
+  return (
+    <div className='bg-ikkiarBgGray2 flex-col justify-center h-screen mx-128 mt-10 rounded-md overflow-hidden'>
+      <div className='flex justify-center pt-52'>
+        <RotateLoader></RotateLoader>
+      </div>
     </div>
   )
 }
--- pages/leaderboard.js
+++ pages/leaderboard.js
@@ -1,9 +1,10 @@
 import summonerService from '../services/summoners'
 import { useState, useEffect } from 'react'
-
+import RotateLoader from "react-spinners/RotateLoader";
 import { motion } from "framer-motion"
 
 const Leaderboard = () => {
+  const [loading, setLoading] = useState(true)
   const [leaderboard, setLeaderboard] = useState([])
 
   useEffect(() => {
@@ -12,54 +13,64 @@ const Leaderboard = () => {
       .then((leadingSummoners) => {
         console.log(leadingSummoners.data)
         setLeaderboard(leadingSummoners.data)
+        setLoading(false)
       })
   }, [])
 
+  if(!loading){
+    return (
+      <div className='bg-ikkiarBgGray2 flex-col justify-center mx-128 mt-10 rounded-md overflow-hidden'>
+
+        <h2 className='text-2xl text-center pt-5'>
+          Leaderboard
+        </h2>
+
+        <div className='flex-col w-auto p-10'>
+          {leaderboard.map((s, idx) =>
+
+
+              <motion.div whileHover={{ scale: 1.06 }} key={'leaderboard-summoner-' + idx} className='flex justify-center border-l-8 border-green rounded-l my-4 bg-ikkiarBgGray1'>
+
+                  {idx === 0 ? 
+                  <div className='flex py-1 my-1'>
+                    <div className='flex'>
+                      <div className='w-96'>
+                        <h2 className='text-xl'>🥇{s.username} (megamonke 🐒👑)</h2>
+                      </div>
+                      <div className='w-40'>
+                        <p className='text-xl text-center font-bold'>&nbsp;{s.points}💠&nbsp;</p>
+                      </div>
+                      <div className='w-24'>
+                        <p className='text-xl text-center font-normal'>{s.wins}W&nbsp;{s.losses}L</p>
+                      </div>
+                    </div>
+                  </div>
+
+                  : 
+                  
+                  <div className='flex py-1 my-1'>
+                      <div className='w-96'>
+                        { idx === 1 || idx === 2 ? <h2 className='text-xl'>{idx ===1 ? '🥈' : '🥉'}{s.username} {idx === 1 ? '(ape-ape 🐵)' : idx===2 ? '(good monke🙊)' : null}</h2> : <h2 className='text-xl'>🏅{s.username}</h2>}
+                      </div>
+                      <div className='w-40'>
+                        <p className='text-xl text-center font-bold'>&nbsp;{s.points}💠&nbsp;</p>
+                      </div>
+                      <div className='w-24'>
+                        <p className='text-xl text-center font-normal'>{s.wins}W&nbsp;{s.losses}L</p>
+                      </div>
+                    </div>
+                  }
+              </motion.div>
+
+          )}
+        </div>
+      </div>
+    )
+  }
   return (
-    <div className='bg-ikkiarBgGray2 flex-col justify-center mx-128 mt-10 rounded-md overflow-hidden'>
-
-      <h2 className='text-2xl text-center'>
-        Leaderboard
-      </h2>
-
-      <div className='flex-col w-auto p-10'>
-        {leaderboard.map((s, idx) =>
-
-
-            <motion.div whileHover={{ scale: 1.06 }} key={'leaderboard-summoner-' + idx} className='flex justify-center border-l-8 border-green rounded-l my-4 bg-ikkiarBgGray1'>
-
-                {idx === 0 ? 
-                <div className='flex py-1 my-1'>
-                  <div className='flex'>
-                    <div className='w-96'>
-                      <h2 className='text-xl'>🥇{s.username} (megamonke 🐒👑)</h2>
-                    </div>
-                    <div className='w-40'>
-                      <p className='text-xl text-center font-bold'>&nbsp;{s.points}💠&nbsp;</p>
-                    </div>
-                    <div className='w-24'>
-                      <p className='text-xl text-center font-normal'>{s.wins}W&nbsp;{s.losses}L</p>
-                    </div>
-                  </div>
-                </div>
-
-                : 
-                
-                <div className='flex py-1 my-1'>
-                    <div className='w-96'>
-                      { idx === 1 || idx === 2 ? <h2 className='text-xl'>{idx ===1 ? '🥈' : '🥉'}{s.username} {idx === 1 ? '(ape-ape 🐵)' : idx===2 ? '(good monke🙊)' : null}</h2> : <h2 className='text-xl'>🏅{s.username}</h2>}
-                    </div>
-                    <div className='w-40'>
-                      <p className='text-xl text-center font-bold'>&nbsp;{s.points}💠&nbsp;</p>
-                    </div>
-                    <div className='w-24'>
-                      <p className='text-xl text-center font-normal'>{s.wins}W&nbsp;{s.losses}L</p>
-                    </div>
-                  </div>
-                }
-            </motion.div>
-
-        )}
+    <div className='bg-ikkiarBgGray2 flex-col justify-center h-screen mx-128 mt-10 rounded-md overflow-hidden'>
+      <div className='flex justify-center pt-52'>
+        <RotateLoader></RotateLoader>
       </div>
     </div>
   )
