d35be1ec26a54dc077452507c8525e553eba92f0
Riku Rainio <rkurainio@gmail.com>
2022-05-16 19:50:08 +0300

    add apply and rules pages

---
+++ pages/apply.js
@@ -0,0 +1,28 @@
+const Apply = () => {
+    return (
+        <div className='bg-ikkiarBgGray2 flex-col content-center justify-center mt-10 rounded-md overflow-hidden h-screen'>
+
+
+            <div className='flex justify-center'>
+                <h1 className="text-3xl font-bold pt-5 text-center">
+                    Apply
+                </h1>
+           
+            </div>
+        
+            <div className='flex justify-center'>
+                <div className='flex-col max-w-md'>
+                    <div className='flex-wrap justify-center pt-4 text-lg'>
+                    Read queue rules before applying <br></br>
+                    Apply to Ikkiar queue <a className='text-md text-discord' href='https://forms.gle/JaYJBxcAzv8U6h1P7'> here 🎮</a>
+                    </div>
+                </div>
+            </div>
+            
+
+
+            </div>
+    )
+}
+
+export default Apply
---
+++ pages/rules.js
@@ -0,0 +1,46 @@
+import summonerService from '../services/summoners'
+import { useState, useEffect } from 'react'
+import RotateLoader from "react-spinners/RotateLoader";
+import { motion } from "framer-motion"
+
+const Rules = () => {
+    return (
+        <div className='bg-ikkiarBgGray2 flex-col justify-center mt-10 rounded-md overflow-hidden h-screen'>
+ 
+        <div className='flex justify-center'>
+            <div className='flex-col justify-center'>
+                <h1 className="text-3xl font-bold pt-5 text-center">
+                    Rules
+                </h1>
+                <div className='flex justify-center pt-5 text-lg'>
+                    Ikkiar queue was created with the intention to get good quality inhouse games
+                    <br></br>
+                    where every player is trying to win. To play in the queue one's values must
+                    <br></br>
+                    align with the following guidelines/ruleset:
+
+
+                    
+
+                </div>
+                <div className='flex justify-center mt-10 text-lg'>
+                    <ul className='list-disc'>
+                        <li>
+                            Queue to your main role
+                        </li>
+                        <li>
+                            Try your hardest in the games
+                        </li>
+                        <li>
+                            Trolling, toxicity & unsportsmanlike behavior is not tolerated
+                        </li>
+                    </ul>
+                </div>
+            </div>
+         </div>
+
+        </div>
+    )
+}
+
+export default Rules
